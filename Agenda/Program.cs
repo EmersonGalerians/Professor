d71c8a1d72e291f8d2294c85e3fdb7f3a2993ce8
EMERSON DE JESUS CANDIDO <emerson.jcandido1@senacsp.edu.br>
2022-11-07 22:11:02 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+
+using Agenda_Lib.DAO.CEP;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VIACep oViaCEP = new VIACep();
+            DadosCEP dadosCEP = new DadosCEP();
+
+            oViaCEP = dadosCEP.PesquisaCEP("06716530");
+            oViaCEP = dadosCEP.PesquisaCEP("05108040");
+            oViaCEP = dadosCEP.PesquisaCEP("06142050");
+            oViaCEP = dadosCEP.PesquisaCEP("06683170");
+            oViaCEP = dadosCEP.PesquisaCEP("06186073");
+            oViaCEP = dadosCEP.PesquisaCEP("06286020");
+            oViaCEP = dadosCEP.PesquisaCEP("06286010");
+            oViaCEP = dadosCEP.PesquisaCEP("06016004");
+            oViaCEP = dadosCEP.PesquisaCEP("06322200");
+            oViaCEP = dadosCEP.PesquisaCEP("06385830");
+            oViaCEP = dadosCEP.PesquisaCEP("06436360");
+            oViaCEP = dadosCEP.PesquisaCEP("05366220");
+            oViaCEP = dadosCEP.PesquisaCEP("06018060");
+            oViaCEP = dadosCEP.PesquisaCEP("06090014");
+            oViaCEP = dadosCEP.PesquisaCEP("06110020");
+            oViaCEP = dadosCEP.PesquisaCEP("06083250");
+            oViaCEP = dadosCEP.PesquisaCEP("06018130");
+            oViaCEP = dadosCEP.PesquisaCEP("06093060");
+            oViaCEP = dadosCEP.PesquisaCEP("06110010");
+            oViaCEP = dadosCEP.PesquisaCEP("06018050");
+            oViaCEP = dadosCEP.PesquisaCEP("06018160");
+            oViaCEP = dadosCEP.PesquisaCEP("06093100");
+            oViaCEP = dadosCEP.PesquisaCEP("06083260");
+            oViaCEP = dadosCEP.PesquisaCEP("06093000");
+            oViaCEP = dadosCEP.PesquisaCEP("06013006");
+            oViaCEP = dadosCEP.PesquisaCEP("06093003");
+            oViaCEP = dadosCEP.PesquisaCEP("06013003");
+            oViaCEP = dadosCEP.PesquisaCEP("06093006");
+            oViaCEP = dadosCEP.PesquisaCEP("06013000");
+            oViaCEP = dadosCEP.PesquisaCEP("06093008");
+            oViaCEP = dadosCEP.PesquisaCEP("06013050");
+            oViaCEP = dadosCEP.PesquisaCEP("06097140");
+            oViaCEP = dadosCEP.PesquisaCEP("06093050");
+            oViaCEP = dadosCEP.PesquisaCEP("06018170");
+            oViaCEP = dadosCEP.PesquisaCEP("06018230");
+            oViaCEP = dadosCEP.PesquisaCEP("06097060");
+            oViaCEP = dadosCEP.PesquisaCEP("06093090");
+            oViaCEP = dadosCEP.PesquisaCEP("06093902");
+            oViaCEP = dadosCEP.PesquisaCEP("06018110");
+            oViaCEP = dadosCEP.PesquisaCEP("06010030");
+            oViaCEP = dadosCEP.PesquisaCEP("06090050");
+            oViaCEP = dadosCEP.PesquisaCEP("06090030");
+            oViaCEP = dadosCEP.PesquisaCEP("06090902");
+            oViaCEP = dadosCEP.PesquisaCEP("06090035");
+            oViaCEP = dadosCEP.PesquisaCEP("06016006");
+            oViaCEP = dadosCEP.PesquisaCEP("06110002");
+            oViaCEP = dadosCEP.PesquisaCEP("06097050");
+            oViaCEP = dadosCEP.PesquisaCEP("06018070");
+            oViaCEP = dadosCEP.PesquisaCEP("06110070");
+            oViaCEP = dadosCEP.PesquisaCEP("06083270");
+            oViaCEP = dadosCEP.PesquisaCEP("06010100");
+            oViaCEP = dadosCEP.PesquisaCEP("06018040");
+            oViaCEP = dadosCEP.PesquisaCEP("06010080");
+            oViaCEP = dadosCEP.PesquisaCEP("06013030");
+            oViaCEP = dadosCEP.PesquisaCEP("06013900");
+            oViaCEP = dadosCEP.PesquisaCEP("06086100");
+            oViaCEP = dadosCEP.PesquisaCEP("06010067");
+            oViaCEP = dadosCEP.PesquisaCEP("06110040");
+            oViaCEP = dadosCEP.PesquisaCEP("06016000");
+            oViaCEP = dadosCEP.PesquisaCEP("06016901");
+            oViaCEP = dadosCEP.PesquisaCEP("06016900");
+            oViaCEP = dadosCEP.PesquisaCEP("06010000");
+            oViaCEP = dadosCEP.PesquisaCEP("06016004");
+            oViaCEP = dadosCEP.PesquisaCEP("06010004");
+            oViaCEP = dadosCEP.PesquisaCEP("06010007");
+            oViaCEP = dadosCEP.PesquisaCEP("06016008");
+            oViaCEP = dadosCEP.PesquisaCEP("06090080");
+            oViaCEP = dadosCEP.PesquisaCEP("06010110");
+            oViaCEP = dadosCEP.PesquisaCEP("06010040");
+            oViaCEP = dadosCEP.PesquisaCEP("06097010");
+            oViaCEP = dadosCEP.PesquisaCEP("06090070");
+            oViaCEP = dadosCEP.PesquisaCEP("06090090");
+            oViaCEP = dadosCEP.PesquisaCEP("06086010");
+            oViaCEP = dadosCEP.PesquisaCEP("06018140");
+            oViaCEP = dadosCEP.PesquisaCEP("06010130");
+            oViaCEP = dadosCEP.PesquisaCEP("06097120");
+            oViaCEP = dadosCEP.PesquisaCEP("06016030");
+            oViaCEP = dadosCEP.PesquisaCEP("06010010");
+            oViaCEP = dadosCEP.PesquisaCEP("06097040");
+            oViaCEP = dadosCEP.PesquisaCEP("06090130");
+            oViaCEP = dadosCEP.PesquisaCEP("06090120");
+            oViaCEP = dadosCEP.PesquisaCEP("06016040");
+            oViaCEP = dadosCEP.PesquisaCEP("06016045");
+            oViaCEP = dadosCEP.PesquisaCEP("06010160");
+            oViaCEP = dadosCEP.PesquisaCEP("06110090");
+            oViaCEP = dadosCEP.PesquisaCEP("06010170");
+            oViaCEP = dadosCEP.PesquisaCEP("06086110");
+            oViaCEP = dadosCEP.PesquisaCEP("06010020");
+            oViaCEP = dadosCEP.PesquisaCEP("06010060");
+            oViaCEP = dadosCEP.PesquisaCEP("06010065");
+            oViaCEP = dadosCEP.PesquisaCEP("06010070");
+            oViaCEP = dadosCEP.PesquisaCEP("06010120");
+            oViaCEP = dadosCEP.PesquisaCEP("06013010");
+            oViaCEP = dadosCEP.PesquisaCEP("06013020");
+            oViaCEP = dadosCEP.PesquisaCEP("06013040");
+            oViaCEP = dadosCEP.PesquisaCEP("06016070");
+            oViaCEP = dadosCEP.PesquisaCEP("06016075");
+            oViaCEP = dadosCEP.PesquisaCEP("06018020");
+            oViaCEP = dadosCEP.PesquisaCEP("06018025");
+            oViaCEP = dadosCEP.PesquisaCEP("06018030");
+            oViaCEP = dadosCEP.PesquisaCEP("06018035");
+            oViaCEP = dadosCEP.PesquisaCEP("06018090");
+            oViaCEP = dadosCEP.PesquisaCEP("06018095");
+            oViaCEP = dadosCEP.PesquisaCEP("06018100");
+            oViaCEP = dadosCEP.PesquisaCEP("06018120");
+            oViaCEP = dadosCEP.PesquisaCEP("06018210");
+            oViaCEP = dadosCEP.PesquisaCEP("06018900");
+            oViaCEP = dadosCEP.PesquisaCEP("06018901");
+            oViaCEP = dadosCEP.PesquisaCEP("06018902");
+            oViaCEP = dadosCEP.PesquisaCEP("06018903");
+            oViaCEP = dadosCEP.PesquisaCEP("06086000");
+            oViaCEP = dadosCEP.PesquisaCEP("06086025");
+            oViaCEP = dadosCEP.PesquisaCEP("06086030");
+            oViaCEP = dadosCEP.PesquisaCEP("06086080");
+            oViaCEP = dadosCEP.PesquisaCEP("06086090");
+            oViaCEP = dadosCEP.PesquisaCEP("06090000");
+            oViaCEP = dadosCEP.PesquisaCEP("06090040");
+            oViaCEP = dadosCEP.PesquisaCEP("06090060");
+            oViaCEP = dadosCEP.PesquisaCEP("06090110");
+            oViaCEP = dadosCEP.PesquisaCEP("06093010");
+            oViaCEP = dadosCEP.PesquisaCEP("06093015");
+            oViaCEP = dadosCEP.PesquisaCEP("06093020");
+            oViaCEP = dadosCEP.PesquisaCEP("06093030");
+            oViaCEP = dadosCEP.PesquisaCEP("06093070");
+            oViaCEP = dadosCEP.PesquisaCEP("06093900");
+            oViaCEP = dadosCEP.PesquisaCEP("06093901");
+            oViaCEP = dadosCEP.PesquisaCEP("06093903");
+            oViaCEP = dadosCEP.PesquisaCEP("06097070");
+            oViaCEP = dadosCEP.PesquisaCEP("06097090");
+            oViaCEP = dadosCEP.PesquisaCEP("06097095");
+            oViaCEP = dadosCEP.PesquisaCEP("06097100");
+            oViaCEP = dadosCEP.PesquisaCEP("06097105");
+            oViaCEP = dadosCEP.PesquisaCEP("06097110");
+            oViaCEP = dadosCEP.PesquisaCEP("06097130");
+            oViaCEP = dadosCEP.PesquisaCEP("06097900");
+            oViaCEP = dadosCEP.PesquisaCEP("06110006");
+            oViaCEP = dadosCEP.PesquisaCEP("06110110");
+
+
+
+
+        }
+    }
+}
